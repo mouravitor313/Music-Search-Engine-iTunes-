ed53ff0b880676ba5e32143384d5ce6ed95e1a86
Vítor Moura <123372633+mouravitor313@users.noreply.github.com>
2023-08-25 14:29:06 -0300

    Update iTunesSearch.py
    User interface modifications

--- iTunesSearch.py
+++ iTunesSearch.py
@@ -6,21 +6,21 @@ def get_term():
         try:
             term = input('What term you want to search? ')
             limit = input('How many results you want? ')
-            response = requests.get(
+            results = requests.get(
                 "https://itunes.apple.com/search?entity=song&limit="+limit+"&term="+term
-                )
-            results = response.json()['results']
+                ).json()['results']
+#            results = response.json()['results']
             return results
         except ValueError:
             print(f'{term} not found!')
 
 def main():
-    os.system('cls')
-    print(' \n***************************')
     results = get_term()
+    os.system('cls')
+    print(' \n***************************\n')
     for c in results:
-        print(f'Name: {c["trackName"]}\nArtist: {c["artistName"]}\nAlbum: {c["collectionName"]}\n ')
-    print(' \n***************************')
+        print(f'Name: {c["trackName"]}\nArtist: {c["artistName"]}\nAlbum: {c["collectionName"]}\n')
+    print('***************************')
 
 main()
  
